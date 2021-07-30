5fd73d64ce23c4aa757183c7167bd704ea20d568
Pavle Prica <pavle.prica5@gmail.com>
2021-07-30 18:53:35 +0200

    Add GmailEmailCredentialsTests

---
+++ src/test/kotlin/io/github/pavleprica/mail/sender/model/GmailEmailCredentialsTests.kt
@@ -0,0 +1,132 @@
+package io.github.pavleprica.mail.sender.model
+
+import io.github.pavleprica.mail.sender.TestNameGenerator
+import io.github.pavleprica.mail.sender.exceptions.InvalidEmail
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+@DisplayNameGeneration(TestNameGenerator::class)
+class GmailEmailCredentialsTests {
+
+    @Nested
+    inner class EmailValidityTests {
+
+        val testPassword = "wkWRWpqRedJj50sW".toCharArray()
+
+        @Nested
+        inner class WhenEmailValid {
+
+            @Test
+            fun shouldPass() {
+                val email = "pavle.prica5@gmail.com"
+
+                EmailCredentials(
+                    email,
+                    testPassword
+                )
+            }
+
+        }
+
+        @Nested
+        inner class WhenEmailInvalid {
+
+            @Test
+            fun shouldThrowInvalidEmailWhenEmailExtensionInvalid() {
+                val email = "pavle.prica5@yahoo.com"
+
+                assertThrows<InvalidEmail> {
+                    EmailCredentials(
+                        email,
+                        testPassword
+                    )
+                }
+            }
+
+            @Test
+            fun shouldThrowInvalidEmailWhenEmailPrefixInvalid() {
+                val email = "@gmail.com"
+
+                assertThrows<InvalidEmail> {
+                    EmailCredentials(
+                        email,
+                        testPassword
+                    )
+                }
+            }
+
+            @Test
+            fun shouldThrowInvalidEmailWhenEmailEmpty() {
+                val email = ""
+
+                assertThrows<InvalidEmail> {
+                    EmailCredentials(
+                        email,
+                        testPassword
+                    )
+                }
+            }
+
+        }
+
+
+    }
+
+    @Nested
+    inner class PasswordValidityTests {
+
+        val testEmail = "artia_munguiavod@gmail.com"
+
+        @Nested
+        inner class WhenPasswordValid {
+
+            @Test
+            fun shouldPass() {
+                EmailCredentials(
+                    testEmail,
+                    "c5hTkNh5amHotzbyL".toCharArray()
+                )
+            }
+
+        }
+
+        @Nested
+        inner class WhenPasswordInvalid {
+
+            @Test
+            fun shouldThrowInvalidPasswordWhenPasswordBlank() {
+                EmailCredentials(
+                    testEmail,
+                    charArrayOf()
+                )
+            }
+
+        }
+
+    }
+
+    @Nested
+    inner class PasswordClearingTests {
+
+        @Test
+        fun shouldAccessPasswordAndClear() {
+            val testingPassword = "OHUJQXA0Nk8LtajlNiN".toCharArray()
+            val credentials = EmailCredentials(
+                "caitlin_putneyo@gmail.com",
+                testingPassword
+            )
+
+            val password = credentials.getPassword()
+
+            assertEquals(testingPassword, password)
+
+            assertNull(credentials.getPassword())
+        }
+
+    }
+
+}
