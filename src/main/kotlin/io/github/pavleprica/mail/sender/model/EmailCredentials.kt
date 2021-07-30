ea2fb5e16614814ba6c69205b14476b6bb1a8fcc
Pavle Prica <pavle.prica5@gmail.com>
2021-07-30 18:26:13 +0200

    Add password clearing after first access
    This is done from security reasons, to avoid the chance of a memory dump
    and password access

--- src/main/kotlin/io/github/pavleprica/mail/sender/model/EmailCredentials.kt
+++ src/main/kotlin/io/github/pavleprica/mail/sender/model/EmailCredentials.kt
@@ -12,6 +12,21 @@ data class EmailCredentials(
     /**
      * Password for auth to the email provider.
      * CharArray is used for improved security.
+     *
+     * Important note is that after the first access to the password it will be cleared from memory.
      */
-    @Suppress("ArrayInDataClass") val password: CharArray
-)
+    @Suppress("ArrayInDataClass") private var password: CharArray?
+) {
+
+    /**
+     * Access the password of the email account. Clearing the password from memory afterwards.
+     */
+    fun getPassword(): CharArray? {
+        val currentPassword = password
+
+        password = null
+
+        return currentPassword
+    }
+
+}
