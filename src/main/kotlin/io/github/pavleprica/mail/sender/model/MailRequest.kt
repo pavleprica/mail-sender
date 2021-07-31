536666e9b102ae344f1cdabdff49c085be3eba3d
Pavle Prica <pavle.prica5@gmail.com>
2021-07-31 13:45:19 +0200

    Add MailRequest validation

--- src/main/kotlin/io/github/pavleprica/mail/sender/model/MailRequest.kt
+++ src/main/kotlin/io/github/pavleprica/mail/sender/model/MailRequest.kt
@@ -1,5 +1,12 @@
 package io.github.pavleprica.mail.sender.model
 
+import io.github.pavleprica.mail.sender.exceptions.AttachmentFileNotFound
+import io.github.pavleprica.mail.sender.exceptions.Content
+import io.github.pavleprica.mail.sender.exceptions.ContentEmpty
+import io.github.pavleprica.mail.sender.exceptions.InvalidEmail
+import org.apache.commons.validator.routines.EmailValidator
+import java.io.File
+
 /**
  * MailRequests represents a single email to be sent.
  */
@@ -23,4 +30,28 @@ data class MailRequest(
      * Paths to files to be attached to the email.
      */
     val attachmentsPaths: List<String>,
-)
+) {
+
+    init {
+        validateReceiverEmail(receiver)
+        validateTitle(title)
+        validateAttachmentFiles(attachmentsPaths)
+    }
+
+    private fun validateReceiverEmail(receiver: String) {
+        if (receiver.isEmailInvalid()) throw InvalidEmail(receiver)
+    }
+
+    private fun String.isEmailInvalid(): Boolean {
+        return !EmailValidator.getInstance().isValid(this)
+    }
+
+    private fun validateTitle(title: String) {
+        if (title.isBlank()) throw ContentEmpty(Content.TITLE)
+    }
+
+    private fun validateAttachmentFiles(attachmentsPaths: List<String>) {
+        attachmentsPaths.forEach { if (!File(it).exists()) throw AttachmentFileNotFound(it) }
+    }
+
+}
